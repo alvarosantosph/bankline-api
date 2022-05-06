638d2cf43e9546d77818b367b0abdadde1e94607
Alvaro Santos <siruz_slipknot@hotmail.com>
2022-05-06 10:05:47 -0300

    Adicionada classe WebConfig e ajustado Controller e Repository Movimentação

--- src/main/java/com/dio/santander/bankline/api/controllers/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controllers/MovimentacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,8 +30,14 @@ public class MovimentacaoController {
 		return repository.findAll();
 	}
 	
+    @GetMapping("/{idConta}")
+	public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+		return repository.findByIdConta(idConta);
+	}
+	
 	@PostMapping
 	public void save(@RequestBody NovaMovimentacao movimentacao) {
 		service.save(movimentacao);
 	}
+	
 }
